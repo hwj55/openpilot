3f7eaa6bb1a4585f64617ad77db8db4f0e1e6032
1okko <136616964+1okko@users.noreply.github.com>
2024-12-24 22:19:40 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
-
+export ATHENA_HOST='wss://athena.springerelectronics.com'
+export API_HOST='https://api.springerelectronics.com'
+export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
+export SKIP_FW_QUERY=1
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
