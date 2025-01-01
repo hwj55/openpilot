488ecf8c8bbba2584352704b9f51167133638aa0
1okko <136616964+1okko@users.noreply.github.com>
2025-01-01 14:10:30 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-export ATHENA_HOST='wss://athena.stable.konik.ai/'
-export API_HOST='https://api.stable.konik.ai'
+export ATHENA_HOST='wss://athena.konik.ai/'
+export API_HOST='https://api.konik.ai'
 export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 export SKIP_FW_QUERY=1
 yes | bash 1.sh
