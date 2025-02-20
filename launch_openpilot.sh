36fae5c1252413859fa65d4158c7456b728ce432
1okko <136616964+1okko@users.noreply.github.com>
2025-02-20 08:26:14 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -3,8 +3,5 @@ export ATHENA_HOST='wss://athena.konik.ai/'
 export API_HOST='https://api.konik.ai'
 export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 export SKIP_FW_QUERY=1
-yes | bash 1.sh
 
-# 删除执行过的脚本
-rm -- 1.sh
 exec ./launch_chffrplus.sh
