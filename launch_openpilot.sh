31e47d97144c7d4b8c959cf37dae79d94284186d
hwj55 <you@example.com>
2026-01-06 14:58:29 +0800

    Apply modifications for dp1.0.3

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-echo -n "2" > /data/params/d/HasAcceptedTerms
-echo -n "0.2.0" > /data/params/d/CompletedTrainingVersion
-echo -n "1" > /data/params/d/IsMetric
+export API_HOST=https://api.konik.ai
+export ATHENA_HOST=wss://athena.konik.ai
+#export MAPS_HOST=https://api.konik.ai/maps
+export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 
 exec ./launch_chffrplus.sh
