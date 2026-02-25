79437c9bdb04f58753e34a6b0ba0786a547cac62
hwj <huangweijun41@gmail.com>
2026-02-25 15:09:52 +0800

    Adjust comfort brake and stop distance parameters

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -53,18 +53,18 @@ T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N) for idx in range(N+1
 T_IDXS = np.array(T_IDXS_LST)
 FCW_IDXS = T_IDXS < 5.0
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
-COMFORT_BRAKE = 2.5
-STOP_DISTANCE = 4.5
+COMFORT_BRAKE = 2.2
+STOP_DISTANCE = 5.5
 CRUISE_MIN_ACCEL = -1.2
-CRUISE_MAX_ACCEL = 1.6
+CRUISE_MAX_ACCEL = 1.4
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 1.0
+    return 1.5
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.0
+    return 1.5
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.5
+    return 1.5
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
