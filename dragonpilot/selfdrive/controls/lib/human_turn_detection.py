a0bfcd01794a6ecfe2e174ca595d9d4dfad56017
herizon1054 <spivbaby@gmail.com>
2026-03-30 02:35:15 +0800

    加快htd解除橫向

--- dragonpilot/selfdrive/controls/lib/human_turn_detection.py
+++ dragonpilot/selfdrive/controls/lib/human_turn_detection.py
@@ -137,7 +137,7 @@ class HumanTurnDetection:
     if condition_met:
       if self._trigger_start_time == 0.0:
         self._trigger_start_time = time.monotonic()
-      elif time.monotonic() - self._trigger_start_time >= 0.2:
+      elif time.monotonic() - self._trigger_start_time >= 0.1:
         return True 
     else:
       self._trigger_start_time = 0.0 
