a9070b81bd5d9b1cc4c91ce657b2ec74b3b75526
herizon1054 <spivbaby@gmail.com>
2026-06-16 03:43:53 +0800

    Update min-feat.lon.dtsc.py

--- dragonpilot/settings/min-feat.lon.dtsc.py
+++ dragonpilot/settings/min-feat.lon.dtsc.py
@@ -7,7 +7,7 @@ ITEMS = [
     "type": "toggle_item",
     "title": lambda: tr("Dynamic Turn Speed Control (DTSC)"),
     "description": lambda: tr("DTSC automatically adjusts the vehicle's predicted speed based on upcoming road curvature and grip conditions.<br>Originally from the openpilot TACO branch."),
-    "condition": "openpilotLongitudinalControl",
+   #"condition": "openpilotLongitudinalControl",
     "flags": "PERSISTENT",
     "param_type": "BOOL",
     "default": "0",
