7e97f5546fc998af00ddfca33cb2adc45874e950
herizon1054 <spivbaby@gmail.com>
2026-06-16 12:22:15 +0800

    Update min-feat.lat.alka-v2.py

--- dragonpilot/settings/min-feat.lat.alka-v2.py
+++ dragonpilot/settings/min-feat.lat.alka-v2.py
@@ -7,7 +7,7 @@ ITEMS = [
     "type": "toggle_item",
     "title": lambda: tr("Always-on Lane Keeping Assist (ALKA)"),
     "description": lambda: tr("Enable lateral control even when ACC/cruise is disengaged, using ACC Main or LKAS button to toggle. Vehicle must be moving."),
-    "brands": ["toyota", "hyundai", "honda", "volkswagen", "subaru", "mazda", "nissan", "ford"],
+    #"brands": ["toyota", "hyundai", "honda", "volkswagen", "subaru", "mazda", "nissan", "ford"],
     "flags": "PERSISTENT",
     "param_type": "BOOL",
     "default": "1",
