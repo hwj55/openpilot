ad122233e6365583ca42c30bdfb8bc741c5381d5
herizon1054 <spivbaby@gmail.com>
2026-06-25 12:37:51 +0800

    Update min-feat.lon.apm.py

--- dragonpilot/settings/min-feat.lon.apm.py
+++ dragonpilot/settings/min-feat.lon.apm.py
@@ -7,7 +7,7 @@ ITEMS = [
     "type": "toggle_item",
     "title": lambda: tr("Adaptive Personality Mode (APM)"),
     "description": lambda: tr("Automatically switches personality to \"Aggressive\" below 0 km/h and restores your selected personality above 5 km/h."),
-    "condition": "openpilotLongitudinalControl",
+    #"condition": "openpilotLongitudinalControl",
     "flags": "PERSISTENT",
     "param_type": "BOOL",
     "default": "0",
