c03110897ff470abe0281423c0ffa0c5d2ceb9f5
hwj <huangweijun41@gmail.com>
2026-06-25 20:16:52 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo -n "2" > /data/params/d/HasAcceptedTerms
+echo -n "0.2.0" > /data/params/d/CompletedTrainingVersion
+echo -n "1" > /data/params/d/IsMetric
 export API_HOST=https://api.konik.ai
 export ATHENA_HOST=wss://athena.konik.ai
 #export MAPS_HOST=https://api.konik.ai/maps
