528a9abe7252ee655e4df2f3408a0ba9c1f43f67
huangwj55 <huangwj55@163.com>
2026-07-31 21:49:57 +0800

    Modification for C3XLite-AGNOS18.4-260731

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
+export API_HOST=https://api.konik.ai
+export ATHENA_HOST=wss://athena.konik.ai
+# Skip onboarding on startup
+echo -n "2" > /data/params/d/HasAcceptedTerms
+echo -n "1.0" > /data/params/d/HasAcceptedTermsSP
+echo -n "0.2.0" > /data/params/d/CompletedTrainingVersion
+echo -n "1.0" > /data/params/d/CompletedSunnylinkConsentVersion  # Sunnylink 同意
+echo -n "1" > /data/params/d/IsMetric
 
-# On any failure, run the fallback launcher
-trap 'exec ./launch_chffrplus.sh' ERR
-C3_LAUNCH_SH="./sunnypilot/system/hardware/c3/launch_chffrplus.sh"
-
-MODEL="$(tr -d '\0' < "/sys/firmware/devicetree/base/model")"
-export MODEL
-
-if [ "$MODEL" = "comma tici" ]; then
-  # Force a failure if the launcher doesn't exist
-  [ -x "$C3_LAUNCH_SH" ] || false
-
-  # If it exists, run it
-  exec "$C3_LAUNCH_SH"
-fi
 
 exec ./launch_chffrplus.sh
--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -54,9 +54,9 @@ T_IDXS = np.array(T_IDXS_LST)
 FCW_IDXS = T_IDXS < 5.0
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 COMFORT_BRAKE = 2.5
-STOP_DISTANCE = 6.0
+STOP_DISTANCE = 4.5
 CRUISE_MIN_ACCEL = -1.2
-CRUISE_MAX_ACCEL = 1.6
+CRUISE_MAX_ACCEL = 2.0
 MIN_X_LEAD_FACTOR = 0.5
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
@@ -76,7 +76,7 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   elif personality==log.LongitudinalPersonality.standard:
     return 1.45
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 1.25
+    return 1.0
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
